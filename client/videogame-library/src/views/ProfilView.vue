49c53c0748258a5ce9edecb51cbabb5fe2c230d6
Leon Raknić <lraknic20@student.foi.hr>
2023-08-08 15:43:49 +0200

    Ažurirana stranica profil

--- client/videogame-library/src/views/ProfilView.vue
+++ client/videogame-library/src/views/ProfilView.vue
@@ -1,24 +1,30 @@
 <template>
-    <div>
+    <div class="profile">
         <h2>Uredi profil</h2>
-        <form @submit.prevent="updateUser">
+        <form v-if="korisnik && korisnik.tip_korisnika_id" @submit.prevent="updateUser">
             <label for="ime">Ime:</label>
-            <input v-model="formData.ime" type="text" id="ime" />
-            <p v-if="formErrors.ime" class="error-message">{{ formErrors.ime }}</p>
-            <br>
+            <InputText v-model="korisnik.ime" type="text" id="ime" :class="{ 'p-invalid': formErrors.ime }" />
+            <span v-if="formErrors.ime" class="error-message">{{ formErrors.ime }}</span>
             <label for="prezime">Prezime:</label>
-            <input v-model="formData.prezime" type="text" id="prezime" />
-            <p v-if="formErrors.prezime" class="error-message">{{ formErrors.prezime }}</p>
-            <br>
+            <InputText v-model="korisnik.prezime" type="text" id="prezime" :class="{ 'p-invalid': formErrors.prezime }" />
+            <span v-if="formErrors.prezime" class="error-message">{{ formErrors.prezime }}</span>
             <label for="korime">Korisničko ime:</label>
-            <input v-model="formData.korime" type="text" id="korime" />
-            <p v-if="formErrors.korime" class="error-message">{{ formErrors.korime }}</p>
-            <br>
+            <InputText v-model="korisnik.korime" type="text" id="korime" :class="{ 'p-invalid': formErrors.korime }" />
+            <span v-if="formErrors.korime" class="error-message">{{ formErrors.korime }}</span>
             <label for="email">Email:</label>
-            <input v-model="formData.email" type="email" id="email" />
-            <p v-if="formErrors.email" class="error-message">{{ formErrors.email }}</p>
-            <p v-if="message">{{ message }}</p>
-            <br>
+            <InputText v-model="korisnik.email" type="email" id="email" :class="{ 'p-invalid': formErrors.email }" />
+            <span v-if="formErrors.email" class="error-message">{{ formErrors.email }}</span>
+            <div v-if="korisnik.datum_istek_bloka">
+                <label for="tipKorisnika">Datum isteka bloka:</label>
+                <InputText v-model="korisnik.datum_istek_bloka" type="text" id="tipKorisnika" disabled />
+            </div>
+            <label for="tipKorisnika">Tip korisnika:</label>
+            <InputText v-model="korisnik.tip_korisnika_id.naziv" type="text" id="tipKorisnika" disabled />
+            <div v-if="zanrovi">
+                <label>Zaduzeni zanrovi:</label>
+                <p>{{ zanrovi }}</p>
+            </div>
+            <span>{{ message }}</span>
             <button>Spremi promjene</button>
         </form>
     </div>
@@ -29,44 +35,53 @@ import { ref, onMounted } from 'vue';
 import type { KorisnikI } from '@/types/KorisnikI';
 import * as yup from 'yup';
 import axiosClient from '@/services/axiosClient';
+import { useToast } from "vue-toastification";
+import InputText from 'primevue/inputtext';
+import moment from 'moment';
 
-const formData = ref({
-    ime: '',
-    prezime: '',
-    email: '',
-    korime: '',
-});
+const toast = useToast();
 
+const korisnik = ref<KorisnikI>();
+const zanrovi = ref<string>();
+const isBlocked = ref<boolean>(false);
 const formErrors = ref<any>({
     ime: '',
     prezime: '',
-    email: '',
     korime: '',
+    email: ''
 });
+
 
 const message = ref();
 
 const schema = yup.object({
     ime: yup.string().required('Ime je obavezno polje.'),
     prezime: yup.string().required('Prezime je obavezno polje.'),
-    email: yup.string().email('Email nije validan.').required('Email je obavezno polje.'),
     korime: yup.string().min(3, 'Korisničko ime mora imati barem 3 znaka').required('Korisničko ime je obavezno polje.'),
+    email: yup.string().email('Email nije validan.').required('Email je obavezno polje.'),
 });
 
 const userID = localStorage.getItem('id');
 
 var getUserData = async () => {
-    try {
-        const response = await axiosClient.get('korisnici/' + userID);
-        const data = await response.data;
-
-        formData.value.ime = data.ime;
-        formData.value.prezime = data.prezime;
-        formData.value.email = data.email;
-        formData.value.korime = data.korime;
-    } catch (error) {
-        message.value = 'Greška pri dohvaćanju korisnika!';
-    }
+    axiosClient
+        .get('korisnici/' + userID)
+        .then((response) => {
+            korisnik.value = response.data;
+            if (korisnik.value?.datum_istek_bloka) {
+                const datumBloka = new Date(korisnik.value.datum_istek_bloka);
+                const trenutniDatum = new Date();
+                if (datumBloka >= trenutniDatum) {
+                    isBlocked.value = true;
+                    korisnik.value.datum_istek_bloka = moment(korisnik.value.datum_istek_bloka).format('DD.MM.YYYY HH:mm');
+                }
+            }
+            if (korisnik.value?.zaduzenZanr)
+                zanrovi.value = korisnik.value.zaduzenZanr.map((zanr) => zanr.naziv).join(', ');
+        })
+        .catch((error) => {
+            message.value = 'Greška pri dohvaćanju korisnika!';
+        });
 };
 
 async function updateUser() {
@@ -74,19 +89,12 @@ async function updateUser() {
         formErrors.value = {};
         message.value = '';
 
-        await schema.validate(formData.value, { abortEarly: false });
+        await schema.validate(korisnik.value, { abortEarly: false });
 
-        const korisnik: KorisnikI = {
-            ime: formData.value.ime,
-            prezime: formData.value.prezime,
-            email: formData.value.email,
-            korime: formData.value.korime,
-        };
-
-        const response = await axiosClient.put(`korisnici/${userID}`, korisnik);
+        const response = await axiosClient.put(`korisnici/${userID}`, korisnik.value);
 
         if (response.status == 200)
-            message.value = "Uspješno ste ažurirali profil!";
+            toast.success('Profil je uspješno ažuriran!');
     } catch (error: any) {
         if (error instanceof yup.ValidationError) {
             error.inner.forEach((e) => {
@@ -102,3 +110,53 @@ async function updateUser() {
 
 onMounted(getUserData);
 </script>
+
+<style scoped>
+.profile {
+    width: 350px;
+    margin: auto;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+}
+
+.profile h2 {
+    text-align: center;
+}
+
+.profile label {
+    display: block;
+    margin-bottom: 5px;
+}
+
+.profile input {
+    width: 100%;
+    padding: 8px;
+    margin-bottom: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+}
+
+.profile span {
+    color: red;
+    font-size: 14px;
+    margin-bottom: 5px;
+    display: block;
+}
+
+.profile button {
+    width: 50%;
+    padding: 10px;
+    background-color: #0d79ec;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    display: grid;
+    margin: auto;
+}
+
+.profile button:hover {
+    background-color: #0556ad;
+}
+</style>
--- server/videogame-library/api/controllers/KorisnikController.js
+++ server/videogame-library/api/controllers/KorisnikController.js
@@ -6,90 +6,106 @@
  */
 
 module.exports = {
-  findAll: async function (req, res) {
-    try {
-      const korisnici = await Korisnik.find().populate('tip_korisnika_id').populate('zaduzenZanr');
-      return res.json(korisnici);
-    } catch (err) {
-      return res.serverError(err);
-    }
-  },
+	findAll: async function (req, res) {
+		try {
+			const korisnici = await Korisnik.find().populate('tip_korisnika_id').populate('zaduzenZanr');
+			return res.json(korisnici);
+		} catch (err) {
+			return res.serverError(err);
+		}
+	},
 
-  findOne: async function (req, res) {
-    try {
-      const korisnik = await Korisnik.findOne({ id: req.params.id }).populate('zaduzenZanr');
-      if (!korisnik) {
-        return res.notFound('Korisnik nije pronađen');
-      }
-      return res.json(korisnik);
-    } catch (err) {
-      return res.serverError(err);
-    }
-  },
+	findOne: async function (req, res) {
+		try {
+			const korisnik = await Korisnik.findOne({ id: req.params.id }).populate('zaduzenZanr').populate('tip_korisnika_id');
+			if (!korisnik) {
+				return res.notFound('Korisnik nije pronađen');
+			}
+			return res.json(korisnik);
+		} catch (err) {
+			return res.serverError(err);
+		}
+	},
 
-  updateUser: async function (req, res) {
-    try {
-      const korisnikId = req.params.id;
-      const updatedKorisnik = {
-        ime: req.body.ime,
-        prezime: req.body.prezime,
-        korime: req.body.korime,
-        email: req.body.email,
-        lozinka: req.body.lozinka
-      };
-      const updated = await Korisnik.updateOne({ id: korisnikId }).set(updatedKorisnik);
-      if (!updated) {
-        return res.status(404).json('Korisnik nije pronađen!');
-      }
-      return res.ok(updated);
-    } catch (err) {
-      return res.serverError(err);
-    }
-  },
+	updateUser: async function (req, res) {
+		try {
+			const korisnikId = req.params.id;
+			const updatedKorisnik = {
+				ime: req.body.ime,
+				prezime: req.body.prezime,
+				korime: req.body.korime,
+				email: req.body.email,
+			};
 
-  updateUserType: async function (req, res) {
-    try {
-      const korisnikId = req.params.id;
-      const tip_korisnika_id = req.body.tip_korisnika_id;
+			const emailRegex = /^\S+@\S+\.\S+$/;
 
-      const korisnik = await Korisnik.findOne({ id: req.params.id });
+			console.log(updatedKorisnik);
+			console.log(req.body.ime);
+			console.log(req.body.prezime);
+			console.log(req.body.korime);
+			console.log(req.body.email);
 
-      if(korisnik.tip_korisnika_id == tip_korisnika_id) {
-        return res.badRequest('Korisnik je već tog tipa koji ste odabrali!');
-      }
+			if (!updatedKorisnik.ime || !updatedKorisnik.prezime || !updatedKorisnik.korime || !updatedKorisnik.email) {
+				return res.status(400).json({ error: 'Sva polja su obavezna!' });
+			}
 
-      const updatedKorisnik = {
-        tip_korisnika_id: tip_korisnika_id
-      };
+			if (!emailRegex.test(email)) {
+				return res.status(400).json({ error: 'Email nije ispravan!' });
+			}
 
-      const updated = await Korisnik.updateOne({ id: korisnikId }).set(updatedKorisnik);
-      if (!updated) {
-        return res.status(404).json('Korisnik nije pronađen!');
-      }
-      
-      return res.ok('Korisnik je ažuriran!');
-    } catch (err) {
-      return res.serverError(err);
-    }
-  },
+			const updated = await Korisnik.updateOne({ id: korisnikId }).set(updatedKorisnik);
+			if (!updated) {
+				return res.status(404).json('Korisnik nije pronađen!');
+			}
+			return res.ok(updated);
+		} catch (err) {
+			return res.serverError(err);
+		}
+	},
 
-  updateUserTimeout: async function (req, res) {
-    try {
-      const korisnikId = req.params.id;
-      const datum_istek_bloka = req.body.datum_istek_bloka;
+	updateUserType: async function (req, res) {
+		try {
+			const korisnikId = req.params.id;
+			const tip_korisnika_id = req.body.tip_korisnika_id;
 
-      const updatedKorisnik = {
-        datum_istek_bloka: datum_istek_bloka
-      };
+			const korisnik = await Korisnik.findOne({ id: req.params.id });
 
-      const updated = await Korisnik.updateOne({ id: korisnikId }).set(updatedKorisnik);
-      if (!updated) {
-        return res.status(404).json('Korisnik nije pronađen!');
-      }
-      
-      return res.ok('Korisnik je uspješno blokiran!');
-    } catch (err) {
-      return res.serverError(err);
-    }
-  }
+			if (korisnik.tip_korisnika_id == tip_korisnika_id) {
+				return res.badRequest('Korisnik je već tog tipa koji ste odabrali!');
+			}
+
+			const updatedKorisnik = {
+				tip_korisnika_id: tip_korisnika_id
+			};
+
+			const updated = await Korisnik.updateOne({ id: korisnikId }).set(updatedKorisnik);
+			if (!updated) {
+				return res.status(404).json('Korisnik nije pronađen!');
+			}
+
+			return res.ok('Korisnik je ažuriran!');
+		} catch (err) {
+			return res.serverError(err);
+		}
+	},
+
+	updateUserTimeout: async function (req, res) {
+		try {
+			const korisnikId = req.params.id;
+			const datum_istek_bloka = req.body.datum_istek_bloka;
+
+			const updatedKorisnik = {
+				datum_istek_bloka: datum_istek_bloka
+			};
+
+			const updated = await Korisnik.updateOne({ id: korisnikId }).set(updatedKorisnik);
+			if (!updated) {
+				return res.status(404).json('Korisnik nije pronađen!');
+			}
+
+			return res.ok('Korisnik je uspješno blokiran!');
+		} catch (err) {
+			return res.serverError(err);
+		}
+	}
 };
