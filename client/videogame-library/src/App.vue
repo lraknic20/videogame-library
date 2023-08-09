1a8f32485827a15b2fb79eead76d5072a7f1a101
Leon Raknić <lraknic20@student.foi.hr>
2023-08-09 12:05:32 +0200

    Dodan globalni css

--- client/videogame-library/src/App.vue
+++ client/videogame-library/src/App.vue
@@ -9,81 +9,59 @@ let loggedIn = ref(isLoggedIn());
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Početna</RouterLink>
-        <RouterLink to="/istrazi">Istraži</RouterLink>
-        <RouterLink to="/izdavaci">Izdavači</RouterLink>
-        <RouterLink to="/najbolje-igre">Najbolje ocijenjene igre</RouterLink>
-        <RouterLink v-if="!loggedIn" to="/prijava">Prijava</RouterLink>
-        <RouterLink v-if="!loggedIn" to="/registracija">Registracija</RouterLink>
-        <RouterLink v-if="loggedIn" to="/odjava">Odjava</RouterLink>
-        <RouterLink v-if="loggedIn" to="/favoriti">Favoriti</RouterLink>
-        <RouterLink v-if="loggedIn" to="/recenzije">Recenzije igara</RouterLink>
-        <RouterLink v-if="loggedIn" to="/profil">Profil</RouterLink>
-        <RouterLink v-if="loggedIn && isAdmin()" to="/admin">Admin</RouterLink>
-        <RouterLink v-if="loggedIn && isModerator()" to="/moderator">Moderator</RouterLink>
-      </nav>
-    </div>
-  </header>
+	<header>
+		<nav>
+			<RouterLink to="/">Početna</RouterLink>
+			<RouterLink to="/istrazi">Istraži</RouterLink>
+			<RouterLink to="/izdavaci">Izdavači</RouterLink>
+			<RouterLink to="/najbolje-igre">Najbolje ocijenjene igre</RouterLink>
+			<RouterLink v-if="!loggedIn" to="/prijava">Prijava</RouterLink>
+			<RouterLink v-if="!loggedIn" to="/registracija">Registracija</RouterLink>
+			<RouterLink v-if="loggedIn" to="/favoriti">Favoriti</RouterLink>
+			<RouterLink v-if="loggedIn" to="/recenzije">Recenzije igara</RouterLink>
+			<RouterLink v-if="loggedIn" to="/profil">Profil</RouterLink>
+			<RouterLink v-if="loggedIn && isAdmin()" to="/admin">Admin</RouterLink>
+			<RouterLink v-if="loggedIn && isModerator()" to="/moderator">Moderator</RouterLink>
+			<RouterLink v-if="loggedIn" to="/odjava">Odjava</RouterLink>
+		</nav>
+	</header>
 
-  <RouterView />
-  <widget-container-modal />
+	<RouterView />
+	<widget-container-modal />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.fade-enter-from,
+.fade-leave-to {
+	opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+	transition: opacity 0.5s ease;
 }
 
 nav {
-  /* width: 100%; */
-  font-size: 12px;
-  text-align: center;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+	margin-left: -1rem;
+	margin-right: -1rem;
+	margin-top: -1rem;
+	margin-bottom: 1rem;
+	display: flex;
+	justify-content: space-around;
+	background-color: rgb(51, 51, 51);
+	padding: 15px;
 }
 
 nav a {
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+	text-decoration: none;
+	color: white;
+	padding: 5px 10px;
+	border-radius: 5px;
+	transition: background-color 0.3s;
 }
 
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-  }
+nav a:hover,
+a.router-link-active {
+	background-color: rgb(85, 85, 85);
 }
 </style>
